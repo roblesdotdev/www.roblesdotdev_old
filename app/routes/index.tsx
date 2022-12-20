ce4867caf0e833bbabdbf706bd377802d2c9a2a8
roblesdotdev <httpsnip@gmial.com>
2022-12-20 15:25:54 -0300

    home message

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -8,7 +8,7 @@ export default function Index() {
   return (
     <div className="flex h-full flex-col items-center justify-center px-6">
       <h1 className="max-w-xl text-center font-mono text-3xl font-bold md:text-5xl">
-        This site is in development stage!
+        This site is under construction!
       </h1>
       <a
         className="mt-4 text-lg text-link-dark underline hover:text-link"
