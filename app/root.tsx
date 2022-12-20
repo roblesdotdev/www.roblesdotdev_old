a2ee10291eccceff4d88e65f3f57c530afbfdf3a
roblesdotdev <httpsnip@gmial.com>
2022-12-20 12:47:40 -0300

    work with errors

--- app/root.tsx
+++ app/root.tsx
@@ -6,11 +6,14 @@ import {
   Outlet,
   Scripts,
   ScrollRestoration,
+  useCatch,
 } from '@remix-run/react'
 import tailwindStyles from '~/styles/tailwind.css'
 import appStyles from '~/styles/app.css'
 import Navbar from '~/components/navbar'
 import Footer from '~/components/footer'
+import { NotFound, ServerError } from './components/errors'
+import type { ReactNode } from 'react'
 
 export const meta: MetaFunction = () => ({
   charset: 'utf-8',
@@ -52,25 +55,58 @@ export const links: LinksFunction = () => [
   { rel: 'stylesheet', as: 'style', href: appStyles },
 ]
 
-export default function App() {
+function Document({
+  title,
+  children,
+}: {
+  title?: string
+  children: ReactNode
+}) {
   return (
     <html lang="en">
       <head>
+        {title ? <title>{title}</title> : null}
         <Meta />
         <Links />
       </head>
       <body className="min-h-screen w-full overflow-x-hidden antialiased">
         <div className="layout bg-canvas font-mono text-fg">
           <Navbar className="navbar" />
-          <main className="main">
-            <Outlet />
-          </main>
+          <main className="main">{children}</main>
           <Footer className="footer" />
         </div>
         <ScrollRestoration />
         <Scripts />
-        <LiveReload />
+        {process.env.NODE_ENV === 'development' ? <LiveReload /> : null}
       </body>
     </html>
+  )
+}
+
+export default function App() {
+  return (
+    <Document>
+      <Outlet />
+    </Document>
+  )
+}
+
+export function CatchBoundary() {
+  const caught = useCatch()
+  if (caught.status === 404) {
+    return (
+      <Document title="Oh No! Not Found">
+        <NotFound />
+      </Document>
+    )
+  }
+  throw new Error(`Unhandled error: ${caught.status}`)
+}
+
+export function ErrorBoundary({ error }: { error: Error }) {
+  return (
+    <Document title="Uh-oh!">
+      <ServerError />
+    </Document>
   )
 }
