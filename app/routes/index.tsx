d6153310cf56d6632d7345fd2ffa2d44d2ba71cf
roblesdotdev <httpsnip@gmial.com>
2022-12-20 09:19:02 -0300

    setup tailwind with postcss

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -1,16 +1,17 @@
 export default function Index() {
   return (
-    <div
-      style={{
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        minHeight: '90vh',
-      }}
-    >
-      <h1 style={{ fontSize: '1.5em', fontWeight: 'lighter' }}>
-        Work in progress
+    <div className="bg-gray-900 px-6 text-slate-300 min-h-screen flex flex-col items-center justify-center">
+      <h1 className="text-3xl md:text-5xl font-bold font-mono max-w-xl text-center">
+        This site is in development stage!
       </h1>
+      <a
+        className="underline text-lg mt-4"
+        href="https://github.com/roblesdotdev/www.roblesdotdev"
+        target="_blank"
+        rel="noreferrer"
+      >
+        Source code
+      </a>
     </div>
   )
 }
