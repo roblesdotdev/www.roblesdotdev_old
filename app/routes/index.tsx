0a5c9b8c8ed1d6927a319ddaa8b99d47445e433c
robles.dev <90198035+roblesdotdev@users.noreply.github.com>
2022-12-20 08:23:13 -0300

    DevExp (#1)

--- app/routes/index.tsx
+++ app/routes/index.tsx
@@ -2,15 +2,15 @@ export default function Index() {
   return (
     <div
       style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        minHeight: "90vh",
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        minHeight: '90vh',
       }}
     >
-      <h1 style={{ fontSize: "1.5em", fontWeight: "lighter" }}>
+      <h1 style={{ fontSize: '1.5em', fontWeight: 'lighter' }}>
         Work in progress
       </h1>
     </div>
-  );
+  )
 }
