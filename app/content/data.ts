5009fd64c141d1165653aff176160c7f2fcb72ea
robles.dev <90198035+roblesdotdev@users.noreply.github.com>
2022-12-21 10:10:51 -0300

    about page (#6)

---
+++ app/content/data.ts
@@ -0,0 +1,27 @@
+export const info = [
+  `Hi! My name is <b>Aldo R. Robles</b> and I'm a passionate developer 
+  interested in web, mobile, command line tools and tons of other 
+  stuff.`.trim(),
+  `I'm currently located in Argentina, I work remotely and do projects 
+  experimenting with different technologies to create better sites and 
+  applications.`.trim(),
+  `I am a Systems Analyst and have done a variety of bootcamps where I have 
+  built various applications with different technologies and frameworks.`.trim(),
+  `While studying, I worked as a freelance, where I developed web applications 
+  with HTML, CSS and Vanilla JS that allowed me to learn all the foundations of 
+  modern websites.`.trim(),
+  `Outside work, my hobbies include playing bass, playing video games, watch 
+  science fiction programs and movies, and play while learning new 
+  technologies.`.trim(),
+]
+
+export const skills = [
+  `My work fits under the umbrella of "Fullstack Developer". With a wide
+  variety of skills focused on web and mobile development among which
+  stand out: HTML, CSS, Javascript, Typescript, React(nextjs, remix),
+  Nodejs, Kotlin, SQL, Prisma and a few more.
+  `.trim(),
+  `I am constantly in search of creating quality software, with great
+  performance and accessible to all people.
+  `.trim(),
+]
