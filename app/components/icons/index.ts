b08d507b6d14f3a974ee9a1b279338b4aa6314bb
robles.dev <90198035+roblesdotdev@users.noreply.github.com>
2022-12-21 15:31:33 -0300

    hero section (#8)

--- app/components/icons/index.ts
+++ app/components/icons/index.ts
@@ -1,1 +1,4 @@
 export { default as AntIcon } from './ant-icon'
+export { default as ArrowOnSquareIcon } from './arrow-on-square-icon'
+export { default as GithubIcon } from './github-icon'
+export { default as LinkedinIcon } from './linkedin-icon'
