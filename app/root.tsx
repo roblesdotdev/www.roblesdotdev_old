4053a9caed0f8157f4909ecd1b18b61e2425bf7f
robles.dev <90198035+roblesdotdev@users.noreply.github.com>
2022-12-22 08:02:05 -0300

    search engines (#9)

--- app/root.tsx
+++ app/root.tsx
@@ -51,6 +51,25 @@ export const links: LinksFunction = () => [
     type: 'font/woff2',
     crossOrigin: 'anonymous',
   },
+  {
+    rel: 'apple-touch-icon',
+    sizes: '180x180',
+    href: '/favicons/apple-touch-icon.png',
+  },
+  {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '32x32',
+    href: '/favicons/favicon-32x32.png',
+  },
+  {
+    rel: 'icon',
+    type: 'image/png',
+    sizes: '16x16',
+    href: '/favicons/favicon-16x16.png',
+  },
+  { rel: 'manifest', href: '/site.webmanifest' },
+  { rel: 'icon', href: '/favicon.ico' },
   { rel: 'stylesheet', as: 'style', href: tailwindStyles },
   { rel: 'stylesheet', as: 'style', href: appStyles },
 ]
